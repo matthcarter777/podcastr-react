11bdbb51f03a43430a2cb885fe60a4d68130b255
Mateus <mateushbsb@gmail.com>
2021-04-23 14:59:22 -0300

    Isolated context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,50 +1,23 @@
-import { useState } from 'react';
-
 import '../styles/global.scss';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
-import { PlayerContext } from '../contexts/PlayerContext';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying ] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
  
   return (
-    <PlayerContext.Provider 
-      value={{ 
-        episodeList, 
-        currentEpisodeIndex, 
-        play, 
-        isPlaying, 
-        togglePlay, 
-        setPlayingState
-    }}>
-      <div  className={styles.appWrapper}>
+    <PlayerContextProvider>
+      <div className={styles.appWrapper}>
         <main>
           <Header />
           <Component {...pageProps} />
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   );
 }
 
